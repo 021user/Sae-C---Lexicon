817db2f08c1b2f3d8a737db324f5970abb72213c
021 Iyad <iyad.guesba@icloud.com>
2023-12-18 14:57:16 +0100

    Initial commit

---
+++ Joueur.h
@@ -0,0 +1,36 @@
+#include "Carte.h"
+struct Joueur
+{
+    int NoOrdre;
+    int main;
+    bool malus = false;
+    
+};
+
+struct Joueurs
+{
+    unsigned int nbJoueurs;
+    Joueur** joueurs;
+};
+
+void initialiser(Joueurs& j, unsigned int c){
+    j.nbJoueurs = c;
+    j.joueurs = new Joueur* [c];
+    for (unsigned int i = 0; i < c; i++)
+    {
+        Joueur* joueur = new Joueur;
+        joueur->NoOrdre = i;
+        //faire main joueur
+        j.joueurs[i] = joueur;
+    }
+};
+
+void detruire(Joueurs& j){
+    for (unsigned int i = 0; i < j.nbJoueurs; i++)
+    {
+        delete j.joueurs[i];
+        j.joueurs[i] = nullptr;
+    } 
+    delete[] j.joueurs;
+    j.joueurs = nullptr;
+}; 
---
+++ Main.cpp
@@ -0,0 +1,11 @@
+#include "Joueur.h"
+#include "Pile.h"
+#include "Carte.h"
+
+int main()
+{
+    unsigned int c = 2;
+
+
+    return 0;
+}
